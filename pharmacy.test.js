152c10c73c6b4c0ac5e42f25d7ab6f72dc8c4aca
edoubart <edouard.barthelemy@gmail.com>
2025-03-12 20:53:55 +0100

    Cleanup sample test for readability.

--- pharmacy.test.js
+++ pharmacy.test.js
@@ -1,9 +1,21 @@
+// Custom Modules
 import { Drug, Pharmacy } from "./pharmacy";
 
 describe("Pharmacy", () => {
   it("should decrease the benefit and expiresIn", () => {
-    expect(new Pharmacy([new Drug("test", 2, 3)]).updateBenefitValue()).toEqual(
-      [new Drug("test", 1, 2)],
-    );
+    // Before
+    const drugTestBefore = new Drug("test", 2, 3);
+    const drugsBefore = [ drugTestBefore ];
+    const pharmacy = new Pharmacy(drugsBefore);
+
+    // Call site
+    const updatedDrugs = pharmacy.updateBenefitValue();
+
+    // After
+    const drugTestAfter = new Drug("test", 1, 2);
+    const drugsAfter = [ drugTestAfter ];
+
+    // Assertion
+    expect(updatedDrugs).toEqual(drugsAfter);
   });
 });
