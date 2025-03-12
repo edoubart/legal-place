7db266c0055932c09ae6a4405f1cf62491a4f713
edoubart <edouard.barthelemy@gmail.com>
2025-03-12 22:52:46 +0100

    Last quick fix as I previously broke the original output.json. :)

--- pharmacy.js
+++ pharmacy.js
@@ -90,7 +90,7 @@ export class Pharmacy {
           this.drugs[i].expiresIn = this.drugs[i].expiresIn - 1;
         //}
       }
-      if (this.drugs[i].expiresIn <= 0) {
+      if (this.drugs[i].expiresIn < 0) {
         if (this.drugs[i].name != "Herbal Tea") {
           if (this.drugs[i].name != "Fervex") {
             if (this.drugs[i].benefit > 0) {
